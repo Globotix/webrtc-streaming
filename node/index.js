075345470846f3148a5db4a16a44d3c0df1f5e8c
Denesh <denesh.siva@globotix.sg>
2022-03-15 10:05:33 +0800

    Adding cleartimeout in case more than one timers are created

--- node/index.js
+++ node/index.js
@@ -1,51 +1,38 @@
-const WebSocket = require('ws');
-const path = require('path');
+const WebSocket = require('ws');           // This is required to connect to a ROS Node on the robot
+const path = require('path');              // ? What is this used for?
 const config = require('dotenv').config(); //For sourcing .env config file
-var express = require('express');
-var http = require('http')
+var express = require('express');          // This is used to serve up the html
 
-// Express is an open sourced server, it's the E --> (MERN)
-var app = express();
-
-// We are going to use ejs (nodejs)
-app.set('view engine', 'ejs');
-
-/////////////////
 // Constants
-/////////////////
 const robot_ip_addr = String(process.env.ROBOT_IP_ADDR) || "192.168.69.101";
 const aws_ip_addr = String(process.env.AWS_IP_ADDR) || "52.74.175.195";
 const http_server_port = process.env.HTTP_PORT || 8011;
 const ws_server_port = process.env.WS_SERVER_PORT || 8012;
 const webrtc_server_port = process.env.WEBRTC_SERVER_PORT || 8013;
+const webrtc_ws_url = 'ws://' + robot_ip_addr + ":" + webrtc_server_port + '/webrtc';
 
-webrtc_ws_url = 'ws://' + robot_ip_addr + ":" + webrtc_server_port + '/webrtc';
+// Set up clients
+let webrtc_ws_client;
+let webrtc_conn_timeout = 1000;
+let timeout_function; 
 
-//Set up servers
+// Set up a websocket server on port 8012
 const ws_router_server = new WebSocket.Server({
   port: ws_server_port,
 });
 
-//Set up clients
-let ws_webrtc_client;
-let webrtc_conn_timeout = 1000;
-
 // Set up express router
+var app = express();                                      // Express is an open sourced server, it's the E --> (MERN)
+app.set('view engine', 'ejs');                            // We are going to use ejs (nodejs)
 app.use(express.static(path.join(__dirname, 'public')));  // Find all your resources in this folder
 app.use(express.json());                                  // Parse requests of content-type - application/json
 app.use(express.urlencoded({ extended: true }));          // Parse requests of content-type - appplication/x-www-form-urlencoded
 
-/////////////////////////
-//HTTP Request handling
-/////////////////////////
-/**
- * GET METHODS
- */
-
+// HTTP Request handling
 app.get('/', function (req, res){
-
   console.log(`UI Client with IP Address ${req.ip} is connected`);
 
+  // As a matter of design define these variables here and pass them down to the javascript via ejs syntaxing
   addr_info = {aws_ip_addr: aws_ip_addr, 
               robot_ip_addr: robot_ip_addr,
               ws_server_port: String(ws_server_port), 
@@ -62,77 +49,73 @@ app.get('/test', function (req, res){
     res.end(`Hello, how have you been dear user? You have requested ${url}`)
 })
 
+app.listen(http_server_port, () => {
+  console.log(`webrtc_router HTTP Server listening on port ${http_server_port}`)
+  console.log(`websocket server on port ${ws_server_port}`)
+})
 
-////////////////////
-//Websocket handling
-////////////////////
+// Simply put, the websocket server receives messages on the socket and immediately relays them to any connected clients
 ws_router_server.on('connection', function connection(ws) {
-
-  //Connection to server can tested with "python3 -m websockets ws://localhost:7071"
+  // Connection to server can tested with "python3 -m websockets ws://localhost:7071"
   console.log(`[WS Router Server] Client connected to websocket port ${ws_server_port}`);
 
   ws.on('message', function message(message) {
     const [err, msg] = safeJsonParse(message);
     if (err) {
       console.log('[WS Router Server] Failed to parse JSON: ' + err.message);
-      // ws_webrtc_connection.send(JSON.stringify({error: "Failed to parse JSON, invalid JSON structure, go suck an egg."}));
     } else {
       console.log('[WS Router Server] Received data, relaying to [WS Webrtc server]: %s', msg);
-      ws_webrtc_client.send(JSON.stringify(msg))
+      webrtc_ws_client.send(JSON.stringify(msg))
     }
   });
 
 });
 
 function connectToWebrtcServer() {
+  webrtc_ws_client = new WebSocket(webrtc_ws_url);
 
-  ws_webrtc_client = new WebSocket(webrtc_ws_url);
-
-  ws_webrtc_client.onopen = function(){
-    console.log(`[WS WebRTC client] connected to WebRTC Server on port ${webrtc_server_port}`);
+  webrtc_ws_client.onopen = function(){
+    console.log(`[WS WebRTC client] Connected to Websocket located on WebRTC Server on port: ${webrtc_server_port}`);
   }
   
-  ws_webrtc_client.onmessage = function(message){
+  webrtc_ws_client.onmessage = function(message){
     const [err, msg] = safeJsonParse(message.data); 
 
     if (err) {
-      console.log('[WS WebRTC client] Failed to parse JSON: ' + err.message);
+      console.log('[WS WebRTC client] Failed to parse JSON from WebRTC Server: ' + err.message);
       return;
-    } 
-    else {
-      console.log('[WS WebRTC client] Received data, relaying to [WS Router clients]: %s', msg);
+    } else {
+      console.log('[WS WebRTC client] Received data: %s via Websocket from WebRTC Server. Relaying this information to all Router clients', msg);
+
       ws_router_server.clients.forEach( function each(client) {
         if (client.readyState === WebSocket.OPEN) {
+          // Send the data received from 'WebRTC server' Websocket to all router clients
           client.send(JSON.stringify(msg));
         }
       })
     }
   }
 
-  ws_webrtc_client.onclose = function(err) {
+  webrtc_ws_client.onclose = function(err) {
     console.log(`[WS WebRTC client] is closed. Attempting reconnection in ${webrtc_conn_timeout} seconds : ${err.reason}`);
-    //WARNING! Delete won't free up memory but allows the garbage collector to find the deleted objects when the memory is low.
-    //Refer to [https://stackoverflow.com/questions/11981634/understanding-object-creation-and-garbage-collection-of-a-nodejs-websocket-serve/11982071#11982071]
-    delete ws_webrtc_client; 
+    // WARNING! Delete won't free up memory but allows the garbage collector to find the deleted objects when the memory is low.
+    // Refer to [https://stackoverflow.com/questions/11981634/understanding-object-creation-and-garbage-collection-of-a-nodejs-websocket-serve/11982071#11982071]
+    delete webrtc_ws_client; 
 
-    setTimeout(function() {
+    // Clear any previous just in case this .onclose event triggers consecutively
+    clearTimeout(timeout_function)
+    timeout_function = setTimeout(function() {
       connectToWebrtcServer();
     }, webrtc_conn_timeout );
   };
 
-  ws_webrtc_client.onerror = function(err) {
+  webrtc_ws_client.onerror = function(err) {
     console.error('[WS WebRTC client] encountered error: ', err.message, 'Closing connection');
-    ws_webrtc_client.close();
+    webrtc_ws_client.close();
   };
-
 }
 
-////////////////////
-// Helper methods
-////////////////////
-
-// Parse JSON into a dictionary object while
-// catching errors
+// Parse JSON into a list and catch errors if necessary
 function safeJsonParse(data){
   try{
     return [null, JSON.parse(data)];
@@ -142,14 +125,5 @@ function safeJsonParse(data){
   }
 }
 
-
-////////////////////
 // Establish Connections
-////////////////////
 connectToWebrtcServer();
-
-app.listen(http_server_port, () => {
-    console.log(`webrtc_router HTTP Server listening on port ${http_server_port}`)
-    console.log(`websocket server on port ${ws_server_port}`)
-})
-
